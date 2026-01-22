cee117d50571c4a38ae66f5e13029e935b6d5a60
John Rackles <john@rackles.io>
2026-01-22 14:23:26 +0100

    update node version

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { tanstackStartCookies } from "better-auth/tanstack-start";
+// import { tanstackStartCookies } from "better-auth/tanstack-start";
 import { db } from "./db";
 import { env } from "./env";
 
@@ -15,5 +15,5 @@ export const auth = betterAuth({
 	},
 	database: drizzleAdapter(db, { provider: "pg" }),
 	experimental: { joins: true },
-	plugins: [tanstackStartCookies()], // make sure this is the last plugin in the array
+	// 	plugins: [tanstackStartCookies()], // make sure this is the last plugin in the array
 });
